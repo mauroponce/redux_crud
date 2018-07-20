5d109f943a24c746964e3666e33b5045ea884ef5
Mauro Ponce <mgp1989@gmail.com>
2018-07-20 00:21:31 -0300

    Add semantic UI and active NavLinks in React Router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Link, Route } from 'react-router-dom';
+import { NavLink, Route } from 'react-router-dom';
 import GamesList from './components/games/games_page';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          <Link to="games">Games</Link>
-        </p>
-
+      <div className="ui container">
+        <div className="ui three item menu">
+          <NavLink className="item" activeClassName="active" exact to="/">Home</NavLink>
+          <NavLink className="item" activeClassName="active" exact to="/games">Games</NavLink>
+          <NavLink className="item" activeClassName="active" exact to="/games/new">Add New Game</NavLink>
+        </div>
         <Route exact path='/games' component={GamesList} />
       </div>
     );
